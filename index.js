32a401650526a1070d4a6140dc84535bb88c3010
Asfahmussarrat <160323759+Asfahmussarrat@users.noreply.github.com>
2024-05-13 10:43:23 -0700

    PROJECT5 CURRENCY CONVERTOR

---
+++ index.js
@@ -0,0 +1,39 @@
+import inquirer from "inquirer";
+// Define the lists of different currencies
+let exchange_rate = {
+    "USD": 1,
+    "EUR": 0.9,
+    "CAD": 1.3,
+    "AUD": 1.65,
+    "JYP": 113,
+    "PKR": 280, // Pakistani Rupees
+};
+//Print the users to convert the currencies  from and to
+let user_answers = await inquirer.prompt([
+    {
+        name: "from_currency",
+        type: "list",
+        message: "select the currency convert from",
+        choices: ["USD", "EUR", "CAD", "AUD", "JYP", "PKR",]
+    },
+    {
+        name: "to_currency",
+        type: "list",
+        message: "select the currency convert to",
+        choices: ["USD", "EUR", "CAD", "AUD", "JYP", "PKR"]
+    },
+    {
+        name: "amount",
+        type: "input",
+        message: "Enter the amount to convert:",
+    }
+]);
+// Apply formula to convert currency
+let from_amount = exchange_rate[user_answers.from_currency];
+let to_amount = exchange_rate[user_answers.to_currency];
+let amount = user_answers.amount;
+//Formula of conversion
+let base_amount = from_amount / amount;
+let converted_amount = base_amount * to_amount;
+//Display the converted amount
+console.log(`converted_amount =${converted_amount.toFixed(2)}`);
